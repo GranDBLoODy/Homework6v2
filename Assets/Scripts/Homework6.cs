c842a9bd0d5ba2dcb96ce070c6abf7ec787e34d2
Rostyk <grandbloodycf@gmail.com>
2017-03-09 20:58:51 +0200

    Homework6v2

---
+++ Assets/Scripts/Homework6.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Homework6 : MonoBehaviour
+{
+    public float Speed;
+    private Rigidbody _rigidbody;
+
+    // Use this for initialization
+
+    // Use this for initialization
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            Vector3 position = this.transform.position;
+            position.x++;
+            transform.position = position;
+        }
+            if (Input.GetKey(KeyCode.V))
+        {
+            Vector3 position = transform.position;
+            position.x--;
+            transform.position = position;
+
+        }
+            if (Input.GetKey(KeyCode.U)) {
+            Vector3 position = transform.position;
+            position.z++;
+            transform.position = position;
+        }
+            
+            if (Input.GetKey(KeyCode.P)) {
+            Vector3 position = transform.position;
+            position.z--;
+            transform.position = position;
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Vector3 position = transform.position;
+            position.y++;
+            transform.position = position;
+        }
+        }
+    }
